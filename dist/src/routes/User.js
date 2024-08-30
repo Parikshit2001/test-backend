76a9f7068cc217ddfe185e755faa5032813006ac
Parikshit <parikshitgupta2001@gmail.com>
2024-08-30 06:27:41 +0530

    removed dist and vercel file

--- dist/src/routes/User.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const User_1 = require("../controllers/User");
-const router = (0, express_1.Router)();
-router.route("/signin").post(User_1.signinUser);
-router.route("/signup").post(User_1.signupUser);
-router.route("/signout").post(User_1.signoutUser);
-exports.default = router;
